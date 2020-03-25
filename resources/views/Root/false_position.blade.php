09b67bfa0010c0deecda97bd14bfccf8a281cfae
kgkk <s5904062636258@email.kmutnb.ac.th>
2020-03-25 21:37:36 +0700

    false position

--- resources/views/Root/false_position.blade.php
+++ resources/views/Root/false_position.blade.php
@@ -1,61 +1,102 @@
 @extends('Navbar')
 @section('body')
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h3 align="center">False Position</h3><br>
-          <form method="get" action="{{url('/show/FalsePosition')}}" align="center">
-            <div class="form-group">
-              <input type="submit" class="btn btn-primary"/>
-            </div>
-          </form>
+<style>
+div.a {
+  text-align: center;
+}
+
+div.b {
+  text-align: left;
+}
+
+div.c {
+  text-align: right;
+}
+
+div.d {
+  text-align: justify;
+}
+</style>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h3 align="center" style="color:Tomato;">False Position</h3><br>
+      <form method="get" action="{{url('/show/FalsePosition')}}" align="center">
+        <div class="form-group">
+          <input type="Submit" class="btn btn-primary"/><p>"กดเพื่อดูผลการคำนวน"
         </div>
-      </div>
+      </form>
     </div>
-    <?php
+  </div>
+</div>
+<?php
 
-    				$EPSILON = 0.01;
+  $EPSILON = 0.01;
 
-    				function func($x)
-    				{
-    						return $x * $x * $x - $x * $x + 2; //x^3-x^2+2
-    				}
+  function func($x)
+  {
+      return $x * $x * $x - $x * $x + 2; //x^3-x^2+2
+  }
 
-    				function bisection($a, $b)
-    				{
-    						global $EPSILON;
-    						if (func($a) * func($b) >= 0)
-    						{
-    								echo "You have not assumed " . "right a and b","\n";
-    								return;
-    						}
+  function falsePosition($XL, $XR)
+  {
+      global $EPSILON;
+      if (func($XL) * func($XR) >= 0)
+      {
+          echo "You have not assumed " . "right a and b","\n";
+          return;
+      }
 
-    						$c = $a;
-    						while (($b - $a) >= $EPSILON)
-    						{
-                    $c = ( $a + $b ) / 2;
+      $XM = $XL;
+      $XMOLD = 0;
+      $Error = 100;
+      while ($Error >= $EPSILON)
+      {
 
-    								if (func($c) == 0.0)
-    										break;
-    								else if (func($c) * func($a) < 0)
-    										$b = $c;
-    								else
-    										$a = $c;
+          $XM = ($XL + $XR) / 2;
+          $Error = abs((($XM*$XMOLD)/$XM)*100);
+          if (func($XM) == 0.0)
+              break;
+          else if (func($XM) * func($XL) < 0)
+              $XR = $XM;
+          else
+              $XL = $XM;
 
-    						}
-    						echo "The value of root is : " , $c;
-    				}
+          $XMOLD = $XM;
 
-    				$a =-2;
-    				$b = 3;
-    				$c = bisection($a, $b);
+          $dataInsert = [
+            'XL'=>$XL,
+            'XR'=>$XR,
+            'XM'=>$XM,
+            'XMOLD'=>$XMOLD,
+            'Error'=>$Error
+          ];
+          //dd($dataInsert);
+          \App\False_Position::insert($dataInsert);
+      }
+  }
 
-    				$dataInsert = [
-    					'XL'=>$a,
-    					'XR'=>$b,
-    					'XM'=>$c
-    				];
-    				//dd($dataInsert);
-    				\App\False_Position::insert($dataInsert);
-    			?>
+  $XL =-2;
+  $XR = 3;
+  $XM = falsePosition($XL, $XR);
+?>
+<div class="col-md-12">
+<div class="a">
+<p style="color:DodgerBlue;">
+การแบ่งครึ่งช่วง (Bisection method) คือการแบ่งออกเป็นสองส่วนเท่ากันในคณิตศาสตร์เป็นวิธีการหารากที่ซ้ำ ๆ การแบ่งออกเป็นสองส่วนเท่ากันช่วงเวลาและจากนั้นเลือกช่วงย่อยซึ่งรากต้องอยู่ในแนวแกน X สำหรับการประมวลผลต่อไป เป็นวิธีที่ง่ายและมีประสิทธิภาพ แต่ก็ยังค่อนข้างช้า ด้วยเหตุนี้มันจึงมักใช้เพื่อหาแนวทางในการแก้ปัญหาโดยประมาณซึ่งใช้เป็นจุดเริ่มต้นของวิธีการบรรจบกันอย่างรวดเร็วมากขึ้น วิธีการนี้เรียกว่าวิธีการลดช่วงเวลา วิธีการค้นหาแบบไบนารี
+</p>
+<p style="color:MediumSeaGreen;">จัดทำโดย 5904062636258 นายปฐมพร โรจน์ฤทธิไกร</p>
+<p style="color:Orange;">เมื่อเข้ามาที่หน้านี้ ระบบจะทำการคำนวนค่าเพื่อเก็บลงไปที่ฐานข้อมูล โดยมีค่าเริ่มต้น XL = -2 , XR = 3 เเละ สมการคือ x^3-x^2+2</p>
+<p>Step1: ทำการเดาจุดสองจุดคือค่า Xl และค่า Xuสมมุติว่าค่า Xl เป็นค่าที่ต่ำกว่าจากนั้นทดสอบว่า f (Xl) f (Xu) < 0 ถ้าไม่ใช้ให้หาจุดใหม่ซึ่งจากขั้นตอนนี้ เรารู้ว่ารากจะต้องอยู่ในช่วงนี้
+<p>Step 2: ทําการประมาณค่า Root ที่ต้องการที่จุดกึ่งกลางระหว่างสองค่าใน Step 1 โดยคํานวณ
+<p>Step 3: หาว่าตอนนี้ Root ที่ต้องการอยู่ในครึ่งไหนดังนี้
+<p>False Position
+<p>3.1 ถ้า f (Xl) f (Xr) < 0 แสดงว่า Root ที่ต้องการอยู่ในครึ่งล่าง ให้ตั้ง Xu= Xr และกลับไปทํา Step 2
+
+<p>3.2 ถ้า f (Xl) f (Xr) > 0 แสดงว่า Root ที่ต้องการอยู่ในครึ่งบน ให้ตั้ง Xl = Xr และกลับไปทํา Step 2
+
+<p>3.3 ถ้า f (Xl) f (Xr) = 0 แสดงว่าคําตอบที่ต้องการเท่ากับ X</p>
+</div>
+
+</div>
 @endsection
